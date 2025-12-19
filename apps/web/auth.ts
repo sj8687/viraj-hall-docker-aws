8fa710d45434878637cdd6f662f47a503c43b2fe
sj8687 <shreyashjadhav59807@gmail.com>
2025-12-19 17:37:40 +0530

    added env tests

--- apps/web/auth.ts
+++ apps/web/auth.ts
@@ -67,6 +67,8 @@ const config: NextAuthConfig = {
           });
         }
 
+                  console.log("ohh secret",baseUrl);
+
         const user = await axios.post(
           `${baseUrl}/add/verify`,
           { email },
@@ -109,6 +111,8 @@ const config: NextAuthConfig = {
         if (!email) {
           throw new Error("Invalid email");
         }
+                          console.log("ohh url",baseUrl);
+
         const res = await axios.post(
           `${baseUrl}/add/user`,
           { email, id, name, image },
