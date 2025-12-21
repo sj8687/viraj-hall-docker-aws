dc58bac4b0fee1d6b9e401d02398d8604bde547e
sj8687 <shreyashjadhav59807@gmail.com>
2025-12-21 16:46:22 +0530

    Update auth.ts

--- apps/web/auth.ts
+++ apps/web/auth.ts
@@ -162,12 +162,9 @@ const config: NextAuthConfig = {
         sameSite: process.env.AUTH_ENV === "development" ? "lax" : "lax",
         path: "/",
         secure: process.env.AUTH_ENV === "development" ? false : true,
-         domain:
-          process.env.DOCKER == "true"
-            ? "localhost"
-            : process.env.AUTH_ENV === "production"
-              ? ".virajmultipurposehall.site"
-              : undefined,
+          domain: process.env.AUTH_ENV === "production"
+                  ? ".virajmultipurposehall.site"
+                  : undefined
 
       },
     },
